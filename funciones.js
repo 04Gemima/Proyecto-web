f39e11e96cef944359e59ebfc1df23510e2d33e9
unknown <gemimoriv@gmail.com>
2022-09-12 12:58:25 -0500

    Agregar carpeta

---
+++ funciones.js
@@ -0,0 +1,23 @@
+
+/*códigos para implementar acciones en las páginas web*/
+
+function mensaje()
+{
+alert("Este es el encabezado general");
+}
+
+function mensaje2()
+{
+alert("Este es el subtítulo");
+}
+
+
+function mensaje3()
+{
+alert("Esta es la columna derecha");
+}
+
+function mensaje4()
+{
+alert("Este es el menú de navegación");
+}
